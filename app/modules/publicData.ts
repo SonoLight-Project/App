10e9f6ff961481ff401cfd202647796260817f8a
Jim-Lin-4549 <jim.lin.4549@outlook.com>
2025-07-25 21:04:41 +0800

    fix(oauth): OAuth 回调参数错误

--- app/modules/publicData.ts
+++ app/modules/publicData.ts
@@ -1,8 +1,5 @@
-export const DISCORD_OAUTH_URI =
-    "https://discord.com/oauth2/authorize?client_id=1396193252138549248&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Faccount%2Foauth-redirect%2Fdiscord&scope=identify";
-
-export const GITHUB_OAUTH_URI =
-    "https://github.com/login/oauth/authorize?client_id=Ov23liGnvXbFkUAOcQ3l&redirect_uri=http://localhost:3000/account/oauth-redirect/github&scope=user";
+export const DISCORD_OAUTH_URI = process.env.DISCORD_OAUTH_URI;
+export const GITHUB_OAUTH_URI = process.env.GITHUB_OAUTH_URI;
 
 export const identityMapper: { [key: number]: string } = {
     0: "普通用户",
@@ -15,4 +12,4 @@ export const identityMapper: { [key: number]: string } = {
     7: "顶尖创作者",
     8: "管理员",
     9: "超级管理员",
-}
+};
